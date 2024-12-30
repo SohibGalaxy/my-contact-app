49d3bc2012a09d60952a505051ab2a9ac5f2e796
SohibGalaxy <sohibfarhan12@gmail.com>
2024-12-30 19:36:19 +0700

    ambil data sukses

---
+++ app/contacts/page.tsx
@@ -0,0 +1,20 @@
+import { CreateButtons } from "@/components/Buttons";
+import ContactTable from "@/components/ContactTable";
+import Search from "@/components/Search";
+
+export default function Contacts() {
+  return (
+   
+        <div className="max-w-screen-md mx-auto mt-5">
+
+            <div className="flex items-center justify-between gap-1 mb-5">
+                <Search/>
+                <CreateButtons/>
+            </div>
+            <ContactTable/>
+
+        </div>
+    
+  )
+}
+
